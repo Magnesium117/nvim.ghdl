3fe73d4e70a4943573721dcc9e83be71dcefb5e2
Marcel Gansfusz <marcel.gansfusz@gmail.com>
2024-10-17 20:39:56 +0200

    5

--- lua/ghdl/ghdlrun.lua
+++ lua/ghdl/ghdlrun.lua
@@ -1,44 +1,24 @@
 local M = {}
 
 local utils = require("platformio.utils")
-
-function M.piobuild()
-  utils.cd_pioini()
-  local command = "ghdl -a" .. utils.extra
+function M.ghdlanalyze()
+  local cmd="ghdl -a"
+  local f=io.open("hld-prj.json","r")
+  json=f:read("a")
+  f:close()
+  local config=utils.jsondecode(json)
+  for file in config.files do
+    cmd=cmd.." "..file.file
+  end
+  utils.ToggleTerminal(cmd, "float")
+end
+function M.ghdlrun(args)
+  local command = "ghdl -r " .. args .. "--vcd=wave.vcd"
   utils.ToggleTerminal(command, "float")
 end
 
-function M.pioupload()
-  utils.cd_pioini()
-  local command = "pio run --target upload" .. utils.extra
+function M.ghdlelaborate(args)
+  local command = "ghdl -e " .. args
   utils.ToggleTerminal(command, "float")
 end
-
-function M.pioclean()
-  utils.cd_pioini()
-  local command = "pio run --target clean" .. utils.extra
-  utils.ToggleTerminal(command, "float")
-end
-
-function M.piorun(arg)
-  if not utils.pio_install_check() then
-    return
-  end
-
-  if arg == nil then
-    arg = "upload"
-  end
-
-  arg = utils.strsplit(arg, "%s")[1]
-  if arg == "upload" then
-    M.pioupload()
-  elseif arg == "build" then
-    M.piobuild()
-  elseif arg == "clean" then
-    M.pioclean()
-  else
-    vim.notify("Invalid argument: build, upload or clean", vim.log.levels.WARN)
-  end
-end
-
 return M
--- plugin/platformio.vim
+++ plugin/platformio.vim
@@ -1,3 +1,4 @@
 command Ghdlinit lua require('ghdl.ghdlinit').ghdlinit()
-command Ghdlcp lua require('ghdl.ghdlcp').ghdlcp()
+command Ghdlanal lua require('ghdl.ghdlrun').ghdlanalyze()
+command -nargs=* Ghdlelab lua require('ghdl.ghdlrun').ghdlelaborate(<f-args>)
 command -nargs=* Ghdlrun lua require('ghdl.ghdlrun').ghdlrun(<f-args>)
